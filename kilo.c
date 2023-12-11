4a1b1370406267fb2aefc73ef735a2bc26533c8f
rllychimp <aryanluharuwala1@gmail.com>
2023-12-12 00:17:45 +0530

    enablerawmode

--- kilo.c
+++ kilo.c
@@ -1,7 +1,23 @@
+#include <stdlib.h>
 #include <unistd.h>
+#include <termios.h>
 
+void enableRawMode(){
+	struct termios raw;
+
+	tcgetattr(STDIN_FILENO, &raw);
+
+	raw.c_lflag &= ~(ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+//the CHO feature causes each key to be printed to the terminal, useful in canonical mode, gets in way when we want to render userinterface in raw mode.
+//this program just doesnt print waht we are typing (writing a password in terminal) 
 int main(){
- char c;
- while(read(STDIN_FILENO, &c, 1)==1);
- return 0;
+
+	enableRawMode();
+	char c;
+	while(read(STDIN_FILENO, &c, 1)==1 && c!='q');
+ 	return 0;
 }
