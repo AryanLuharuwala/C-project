bdada208958745bd664cea89a5ca9a052c913529
AryanLuharuwala <aryanluharuwala@kgpian.iitkgp.ac.in>
2023-12-23 02:49:09 +0530

    Moving Cursor

--- kilo.c
+++ kilo.c
@@ -20,6 +20,14 @@ struct editorConfig{
 struct editorConfig E;
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k)((k)& 0x1f)
+
+enum editorKey{
+	ARROW_LEFT=1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN
+};
+
 //CTRL_KEY macro bitwishe-ANDs a character with 00011111 in binary
 void die(const char *s){
 
@@ -84,13 +92,30 @@ void abFree(struct abuf *ab){
 
 
 
-char editorReadKey(){
+int editorReadKey(){
 	int nread;
 	char c;
 	while((nread=read(STDIN_FILENO, &c, 1))!=1){
 		if (nread== -1 && errno !=EAGAIN) die("read");
 	}
-	return c;
+
+	if (c=='\x1b'){
+		char seq[3];
+		if(read(STDIN_FILENO,&seq[0],1)!=1)return '\x1b';
+		if(read(STDIN_FILENO,&seq[1],1)!=1)return '\x1b';
+		
+		if (seq[0]=='['){
+			switch(seq[1]){
+				case 'A':return ARROW_UP;
+				case 'B':return ARROW_DOWN;
+				case 'C':return ARROW_RIGHT;
+				case 'D':return ARROW_LEFT;
+			}
+		}
+		return '\x1b';
+	}else{
+		return c;
+	}
 }
 		
 void editorDrawRows(struct abuf *ab){
@@ -151,6 +176,22 @@ int getWindowSize(int *rows, int *cols){
 	}
 }
 
+void editorMoveCursor(int key){
+	switch(key){
+		case ARROW_LEFT:
+			E.cx--;
+			break;
+		case ARROW_RIGHT:
+			E.cx++;
+			break;
+		case ARROW_UP:
+			E.cy--;
+			break;
+		case ARROW_DOWN:
+			E.cy++;
+			break;
+	}
+}
 
 void editorRefreshScreen(){
 	struct abuf ab = ABUF_INIT;
@@ -172,13 +213,19 @@ void editorRefreshScreen(){
 }
 
 void editorProcessKeypress(){
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch(c){
 		case CTRL_KEY('q'):
 			write(STDOUT_FILENO, "\x1b[2J", 4);
         		write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+		case ARROW_UP:
+		case ARROW_RIGHT:
+		case ARROW_LEFT:
+		case ARROW_DOWN:
+			editorMoveCursor(c);
 			break;
 	}
 }
