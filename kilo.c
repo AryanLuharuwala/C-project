e0a4ad4e8f527dc81f1cdfc240f72e7122ed06c3
AryanLuharuwala <aryanluharuwala@kgpian.iitkgp.ac.in>
2023-12-23 23:51:16 +0530

    commit

--- kilo.c
+++ kilo.c
@@ -25,7 +25,12 @@ enum editorKey{
 	ARROW_LEFT=1000,
 	ARROW_RIGHT,
 	ARROW_UP,
-	ARROW_DOWN
+	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 //CTRL_KEY macro bitwishe-ANDs a character with 00011111 in binary
@@ -105,15 +110,38 @@ int editorReadKey(){
 		if(read(STDIN_FILENO,&seq[1],1)!=1)return '\x1b';
 		
 		if (seq[0]=='['){
+			if (seq[1]>='0' && seq[1]<='9'){
+				if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+				if (seq[2] == '~'){
 			switch(seq[1]){
+				case '1': return HOME_KEY;
+				case '3': return DEL_KEY;
+				case '4': return END_KEY;
+				case '5': return PAGE_UP;
+				case '6': return PAGE_DOWN;
+				case '7': return HOME_KEY;
+				case '8': return END_KEY;
+			}
+				}
+			}else{
+				switch (seq[1]){
 				case 'A':return ARROW_UP;
 				case 'B':return ARROW_DOWN;
 				case 'C':return ARROW_RIGHT;
 				case 'D':return ARROW_LEFT;
+				case 'H':return HOME_KEY;
+				case 'F':return END_KEY;
 			}
 		}
+		}else if (seq[0]=='O'){
+			switch(seq[1]){
+				case 'H':return HOME_KEY;
+				case 'F':return END_KEY;
+			}
+		}
+
 		return '\x1b';
-	}else{
+	} else{
 		return c;
 	}
 }
@@ -179,16 +207,24 @@ int getWindowSize(int *rows, int *cols){
 void editorMoveCursor(int key){
 	switch(key){
 		case ARROW_LEFT:
-			E.cx--;
+			if(E.cx!=0){
+				E.cx--;
+			}
 			break;
 		case ARROW_RIGHT:
-			E.cx++;
+			if(E.cx!=E.screencols-1){
+				E.cx++;
+			}
 			break;
 		case ARROW_UP:
+			if(E.cy!=0){
 			E.cy--;
+			}
 			break;
 		case ARROW_DOWN:
+			if(E.cy!=E.screenrows-1){
 			E.cy++;
+			}
 			break;
 	}
 }
@@ -220,6 +256,22 @@ void editorProcessKeypress(){
 			write(STDOUT_FILENO, "\x1b[2J", 4);
         		write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+
+		case END_KEY:
+			E.cx = E.screencols-1;
+			break;		
+		
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				int times = E.screenrows;
+					while(times--)
+						editorMoveCursor(c==PAGE_UP ? ARROW_UP :ARROW_DOWN);
+			}
 			break;
 		case ARROW_UP:
 		case ARROW_RIGHT:
